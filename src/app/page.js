f2958dbfbe510095a22c9f4b22e817c3e9a88c18
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-11-11 23:54:54 +0600

    fff

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import SelectedTrendingProducts from "./components/SelectedTrendingProducts";
 import InstrumentsAndMeters from "./components/InstrumentsAndMeters";
 import IndustrialEquipmentComponents from "./components/IndustrialEquipmentComponents";
 import ElectricalElectronics from "./components/Electrical&Electronics";
+import EasySourcing from "./components/EasySourcing";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <InstrumentsAndMeters></InstrumentsAndMeters>
       <IndustrialEquipmentComponents></IndustrialEquipmentComponents>
       <ElectricalElectronics></ElectricalElectronics>
+      <EasySourcing></EasySourcing>
     </div>
   );
 }
